60502f6cf631bb7fa49590c1766d925a8b68b7e7
vypq <vypq@rever.vn>
2023-10-30 11:48:07 +0700

    debug

--- rml/vision/object_detection/models/yolov8/ultralytics/engine/validator.py
+++ rml/vision/object_detection/models/yolov8/ultralytics/engine/validator.py
@@ -135,8 +135,8 @@ class BaseValidator:
                 self.args.batch = 1  # export.py models default to batch-size 1
                 LOGGER.info(f'Forcing batch=1 square inference (1,3,{imgsz},{imgsz}) for non-PyTorch models')
 
-            if isinstance(self.args.data, str) and self.args.data.split('.')[-1] in ('yaml', 'yml'):
-                self.data = check_det_dataset([self.args.data])
+            if isinstance(self.args.data, str) and self._check_valid_datasets(self.args.data):
+                self.data = check_det_dataset(self.args.data)
             elif self.args.task == 'classify':
                 self.data = check_cls_dataset(self.args.data, split=self.args.split)
             else:
@@ -323,3 +323,11 @@ class BaseValidator:
     def eval_json(self, stats):
         """Evaluate and return JSON format of prediction statistics."""
         pass
+
+    def _check_valid_datasets(self, datasets) -> bool:
+        is_valid = False
+        for dataset in datasets:
+            is_valid = dataset.split('.')[-1] in ('yaml', 'yml')
+            if is_valid is False:
+                return is_valid
+        return is_valid
