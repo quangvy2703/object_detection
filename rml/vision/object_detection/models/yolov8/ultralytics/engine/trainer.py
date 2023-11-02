90c962b0db214a348afa10e69c8b6ebde54bb8bc
vypq <vypq@rever.vn>
2023-11-02 14:08:51 +0700

    update

--- rml/vision/object_detection/models/yolov8/ultralytics/engine/trainer.py
+++ rml/vision/object_detection/models/yolov8/ultralytics/engine/trainer.py
@@ -561,11 +561,11 @@ class BaseTrainer:
 
     def save_metrics(self, metrics):
         """Saves training metrics to a CSV file."""
-        # keys, vals = list(metrics.keys()), list(metrics.values())
-        # n = len(metrics) + 1  # number of cols
-        # s = '' if self.csv.exists() else (('%23s,' * n % tuple(['epoch'] + keys)).rstrip(',') + '\n')  # header
-        # with open(self.csv, 'a') as f:
-        #     f.write(s + ('%23.5g,' * n % tuple([self.epoch + 1] + vals)).rstrip(',') + '\n')
+        keys, vals = list(metrics.keys()), list(metrics.values())
+        n = len(metrics) + 1  # number of cols
+        s = '' if self.csv.exists() else (('%23s,' * n % tuple(['epoch'] + keys)).rstrip(',') + '\n')  # header
+        with open(self.csv, 'a') as f:
+            f.write(s + ('%23.5g,' * n % tuple([self.epoch + 1] + vals)).rstrip(',') + '\n')
 
         saved_metrics = dict(zip(
             [metric for metric in self.args.metrics],
