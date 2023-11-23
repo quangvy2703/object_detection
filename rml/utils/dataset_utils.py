c59dae2fb86b3b46cb42fd81a25d115e7816939b
vypq <vypq@rever.vn>
2023-11-23 15:53:08 +0700

    update

--- rml/utils/dataset_utils.py
+++ rml/utils/dataset_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
-
+from typing import List, Dict
+from tqdm import tqdm
 
 def load_open_image_metadata(open_image_dir: str, split: str):
     classes_mapping_df = pd.read_csv(
@@ -22,4 +23,21 @@ def load_open_image_metadata(open_image_dir: str, split: str):
         classes_mapping[class_id] = class_name
 
     return classes_mapping, labels
+
+
+def plot_classes(data_dirs: List[str], mapping_names: List[Dict[int, int]]):
+    labels = {}
+    for data_idx, data_dir in enumerate(data_dirs):
+        for label_file in tqdm(os.listdir(data_dir)):
+            with open(os.path.join(data_dir, label_file)) as f:
+                lines = f.readlines()
+                for line in lines:
+                    label = int(line.split(" ")[0])
+                    label = mapping_names[data_idx][label]
+                    if label in labels:
+                        labels[label] += 1
+                    else:
+                        labels[label] = 0
+
+    return labels
 
